313d76c9a3ffbdcd058f0dbcf0b158893f4e5ee2
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-21 09:51:15 -0400

    Create - início da construção do microservices de cadastro de Clientes.

---
+++ devvader-clientes/src/main/java/com/devvaderclientes/DevvaderClientesApplication.java
@@ -0,0 +1,13 @@
+package com.devvaderclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevvaderClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevvaderClientesApplication.class, args);
+	}
+
+}
---
+++ devvader-clientes/src/test/java/com/devvaderclientes/DevvaderClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devvaderclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevvaderClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
