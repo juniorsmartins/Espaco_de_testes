8e63716d6e826b07db75bf65efe91f0f3dfbb2c1
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-21 13:43:01 -0400

    Create - configuração do ModelMapper.

---
+++ devvader-clientes/src/main/java/com/devvaderclientes/domain/configs/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package com.devvaderclientes.domain.configs;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
