1a5d0326508f5d1fefc636a8c767c55eabf7213f
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-22 11:02:52 -0400

    Create - construção de exceção personalizada para recursos não encontrados no database.

---
+++ devvader-clientes/src/main/java/com/devvaderclientes/domain/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.devvaderclientes.domain.exceptions;
+
+public final class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
