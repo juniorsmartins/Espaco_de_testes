67d373f9ed33751ead3ce0a46f00c9a46b51ca91
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-22 11:00:39 -0400

    Create - construção de classe para mensagens padronizadas do sistema.

---
+++ devvader-clientes/src/main/java/com/devvaderclientes/domain/exceptions/MensagemPadronizada.java
@@ -0,0 +1,6 @@
+package com.devvaderclientes.domain.exceptions;
+
+public final class MensagemPadronizada {
+
+    public static final String RECURSO_NAO_ENCONTRADO = "Recurso não encontrado no banco de dados.";
+}
