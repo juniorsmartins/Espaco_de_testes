f75ce5dfc024826da99162277319752b230d850a
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-23 15:43:49 -0400

    Create - implementação do Spring Cloud Netflix.

--- devvader-clientes/src/main/java/com/devvaderclientes/DevvaderClientesApplication.java
+++ devvader-clientes/src/main/java/com/devvaderclientes/DevvaderClientesApplication.java
@@ -2,12 +2,13 @@ package com.devvaderclientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DevvaderClientesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevvaderClientesApplication.class, args);
 	}
-
 }
