67b10551c9934a9fcf052cf0e0c7833c35d69360
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-23 17:10:40 -0400

    Alter - comentários para facilitar estudo posterior.

--- devvader-eureka-server/src/main/java/com/devvadereurekaserver/DevvaderEurekaServerApplication.java
+++ devvader-eureka-server/src/main/java/com/devvadereurekaserver/DevvaderEurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer // Spring Cloud Netflix - Ativa a aplicação como servidor Eureka
 public class DevvaderEurekaServerApplication {
 
 	public static void main(String[] args) {
