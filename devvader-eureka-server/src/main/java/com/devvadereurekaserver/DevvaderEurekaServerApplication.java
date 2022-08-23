4b429306dd0a5a63a0fd72916c57ca80919c15c0
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-23 09:56:57 -0400

    Create - construção do Eureka Server para mapear rotas.

---
+++ devvader-eureka-server/src/main/java/com/devvadereurekaserver/DevvaderEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.devvadereurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevvaderEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevvaderEurekaServerApplication.class, args);
+	}
+
+}
---
+++ devvader-eureka-server/src/test/java/com/devvadereurekaserver/DevvaderEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devvadereurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevvaderEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
