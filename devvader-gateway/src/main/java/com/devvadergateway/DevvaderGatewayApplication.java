8be9316c9d5d136d154e69e9f2fcd28d4104000e
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-24 07:51:41 -0400

    Alter - construção do Gateway (ponto único de entrada).

--- devvader-gateway/src/main/java/com/devvadergateway/DevvaderGatewayApplication.java
+++ devvader-gateway/src/main/java/com/devvadergateway/DevvaderGatewayApplication.java
@@ -2,12 +2,13 @@ package com.devvadergateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DevvaderGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevvaderGatewayApplication.class, args);
 	}
-
 }
