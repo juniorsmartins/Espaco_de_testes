579f6e13907fe5692ce1b205ea2b743331d2ab3a
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-24 13:36:24 -0400

    Create - construção da fetura de comunicação (OpenFeign) entre microservices.

--- devvader-noticias/src/main/java/com/devvadernoticias/domain/services/NoticiaServiceImpl.java
+++ devvader-noticias/src/main/java/com/devvadernoticias/domain/services/NoticiaServiceImpl.java
@@ -3,9 +3,9 @@ package com.devvadernoticias.domain.services;
 import com.devvadernoticias.domain.dtos.request.NoticiaDtoIn;
 import com.devvadernoticias.domain.dtos.response.NoticiaDtoOut;
 import com.devvadernoticias.domain.entities.NoticiaEntity;
+import com.devvadernoticias.domain.http.DevVaderClientes;
 import com.devvadernoticias.domain.ports.INoticiaService;
 import com.devvadernoticias.infra.INoticiaRepository;
-import com.netflix.discovery.converters.Auto;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,13 +23,16 @@ public final class NoticiaService implements INoticiaService {
     @Autowired
     private INoticiaRepository iNoticiaRepository;
 
+    @Autowired
+    private DevVaderClientes devVaderClientes;
+
     @Override
     public ResponseEntity<?> criar(NoticiaDtoIn noticiaDtoIn) {
 
         final var noticiaDeSaida = Optional.of(noticiaDtoIn)
                 .map(noticiaDeEntrada -> modelMapper.map(noticiaDeEntrada, NoticiaEntity.class))
                 .map(noticiaEntityParaSalvar -> {
-                    // verificar existência de cliente em outro microservices
+                    Optional.of(devVaderClientes.consultarClientePorId(noticiaEntityParaSalvar.getCliente())).orElseThrow();
                     return iNoticiaRepository.saveAndFlush(noticiaEntityParaSalvar);})
                 .map(noticiaEntitySalva -> modelMapper.map(noticiaEntitySalva, NoticiaDtoOut.class))
                 .orElseThrow();
