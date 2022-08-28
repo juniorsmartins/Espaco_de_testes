d26d71d07a5eec84587734a24b7299859504c36c
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-28 18:08:48 -0400

    Create - construção de microservice DevVader-Cursos.

---
+++ devvader-cursos/src/main/java/com/devvadercursos/DevvaderCursosApplication.java
@@ -0,0 +1,13 @@
+package com.devvadercursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevvaderCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevvaderCursosApplication.class, args);
+	}
+
+}
---
+++ devvader-cursos/src/test/java/com/devvadercursos/DevvaderCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devvadercursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevvaderCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
