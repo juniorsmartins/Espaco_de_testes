8fdb28388ae47ebf507812ae670003f9ae306627
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-05 08:52:55 -0400

    Configuração de Logback.xml

--- devvader-cursos/src/main/java/com/devvadercursos/DevvaderCursosApplication.java
+++ devvader-cursos/src/main/java/com/devvadercursos/DevvaderCursosApplication.java
@@ -1,5 +1,7 @@
 package com.devvadercursos;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,8 +10,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient // Spring Cloud Netflix - informa à aplicação da necessidade de se registrar num server Eureka
 public class DevvaderCursosApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(DevvaderCursosApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Aplicação iniciada!");
 		SpringApplication.run(DevvaderCursosApplication.class, args);
+		logger.info("Microserviço para cadastrar cursos em Portfólio.");
 	}
 
 }
