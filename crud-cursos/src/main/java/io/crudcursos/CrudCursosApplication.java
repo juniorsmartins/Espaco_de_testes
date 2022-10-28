30fd0b171a50394f938f600b72134f3724160af7
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-28 07:09:11 -0400

    Incício da construção de microservices: crud-cursos.

---
+++ crud-cursos/src/main/java/io/crudcursos/CrudCursosApplication.java
@@ -0,0 +1,13 @@
+package io.crudcursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCursosApplication.class, args);
+	}
+
+}
---
+++ crud-cursos/src/test/java/io/crudcursos/CrudCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.crudcursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
