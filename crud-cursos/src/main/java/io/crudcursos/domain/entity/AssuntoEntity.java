534ea55c59cca98a7e15de5557b06eaf52923113
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-30 15:34:21 -0400

    Mapeamento de entidades Assunto e Curso.

---
+++ crud-cursos/src/main/java/io/crudcursos/domain/entity/AssuntoEntity.java
@@ -0,0 +1,32 @@
+package io.crudcursos.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "assuntos")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@ToString
+public final class AssuntoEntity implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "assunto")
+    private String assunto;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "assunto")
+    private List<CursoEntity> cursos;
+}
