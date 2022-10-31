5fb60e53d04bf2592c9ff09a3abfa52c309e2c16
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-31 08:32:09 -0400

    TDD - início da construção das funcionalidades pelos testes.

--- crud-cursos/src/main/java/io/crudcursos/domain/controller/AssuntoController.java
+++ crud-cursos/src/main/java/io/crudcursos/domain/controller/AssuntoController.java
@@ -2,6 +2,36 @@ package io.crudcursos.domain.controller;
 
 import io.crudcursos.domain.dto.AssuntoDTO;
 import io.crudcursos.domain.entity.filtros.AssuntoFiltro;
+import io.crudcursos.domain.entity.filtros.IFiltro;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
 public final class AssuntoController implements IController<AssuntoDTO, AssuntoFiltro, Long> {
+
+    @Override
+    public ResponseEntity<AssuntoDTO> criar(AssuntoDTO dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Page<AssuntoDTO>> buscarTodos(Pageable pageable, AssuntoFiltro filtro) {
+        return null;
+    }
+
+
+    @Override
+    public ResponseEntity<AssuntoDTO> consultarPorId(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<AssuntoDTO> atualizar(Long aLong, AssuntoDTO dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> deletarPorId(Long aLong) {
+        return null;
+    }
 }
--- crud-cursos/src/test/java/io/crudcursos/domain/controller/AssuntoControllerTest.java
+++ crud-cursos/src/test/java/io/crudcursos/domain/controller/AssuntoControllerTest.java
@@ -1,9 +1,40 @@
 package io.crudcursos.domain.controller;
 
+import io.crudcursos.domain.dto.AssuntoDTO;
+import io.crudcursos.domain.entity.filtros.AssuntoFiltro;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class AssuntoControllerTest {
 
+    private AssuntoDTO assuntoDTO1;
 
+    @Autowired
+    private IController<AssuntoDTO, AssuntoFiltro, Long> controller;
+
+    @BeforeEach
+    void criadorDeCenariosParaTeste() {
+        assuntoDTO1 = AssuntoDTO.builder()
+                .assunto("Java")
+                .build();
+    }
+
+    @Test
+    void teste1_retornar201QuandoCadastrar() {
+        var response = controller.criar(assuntoDTO1);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(ResponseEntity.class, response);
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(AssuntoDTO.class, (response.getBody()).getClass());
+        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        Assertions.assertNotNull(response.getBody().getId());
+        Assertions.assertEquals(assuntoDTO1.getAssunto(), response.getBody().getAssunto());
+    }
 }
