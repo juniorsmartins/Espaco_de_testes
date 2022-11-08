48b6114925c251aaa37f970af197e85b710f7ad0
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-11-08 10:20:00 -0400

    Bug não resolvido na funcionalidade de BuscarTodos.

--- crud-cursos/src/main/java/io/crudcursos/domain/controller/CursoController.java
+++ crud-cursos/src/main/java/io/crudcursos/domain/controller/CursoController.java
@@ -7,9 +7,15 @@ import io.crudcursos.domain.service.AService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "${app.api.base}/cursos", produces = {"application/json"})
@@ -19,27 +25,28 @@ public final class CursoController extends AController<CursoDTO, CursoFiltro, Lo
     private AService<CursoDTO, CursoEntity, CursoFiltro, Long> service;
 
     @Override
-    public ResponseEntity<CursoDTO> criar(CursoDTO dto) {
+    public ResponseEntity<CursoDTO> criar(@RequestBody @Valid CursoDTO dto) {
         return this.service.criar(dto);
     }
 
     @Override
-    public ResponseEntity<Page<CursoDTO>> buscarTodos(CursoFiltro filtro, Pageable paginacao) {
+    public ResponseEntity<Page<CursoDTO>> buscarTodos(CursoFiltro filtro,
+          @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao) {
         return this.service.buscarTodos(filtro, paginacao);
     }
 
     @Override
-    public ResponseEntity<CursoDTO> consultarPorId(Long id) {
+    public ResponseEntity<CursoDTO> consultarPorId(@PathVariable(value = "id") Long id) {
         return this.service.consultarPorId(id);
     }
 
     @Override
-    public ResponseEntity<CursoDTO> atualizar(Long id, CursoDTO dto) {
+    public ResponseEntity<CursoDTO> atualizar(@PathVariable(value = "id") Long id, @RequestBody @Valid CursoDTO dto) {
         return this.service.atualizar(id, dto);
     }
 
     @Override
-    public ResponseEntity<?> deletarPorId(Long id) {
+    public ResponseEntity<?> deletarPorId(@PathVariable(value = "id") Long id) {
         return this.service.deletarPorId(id);
     }
 }
